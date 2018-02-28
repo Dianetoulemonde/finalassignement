61120f95f63e6a1076cf3bc85c72a050a6285633
Dianetoulemonde <diane.toulemonde@gmail.com>
2018-02-28 15:43:20 +0100

    jsmodif

--- cocktailapi.js
+++ cocktailapi.js
@@ -1,3 +1,11 @@
 $(function () {
-    alert("whatever");
+    $("#searchButton").on("click", () => {
+        let name = $("#names").val();
+        let ingredients = $("#ingredients").val();
+    
+    $ajax({
+        url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+    }).done(response => {
+        let res = JSON.parse(response);
+    })
 });
