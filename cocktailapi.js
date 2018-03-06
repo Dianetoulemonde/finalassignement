ebe433ed94029f68e9db1d68f6247ebe7791f8bc
Dianetoulemonde <diane.toulemonde@gmail.com>
2018-03-06 19:33:32 +0100

    js push

--- cocktailapi.js
+++ cocktailapi.js
@@ -1,24 +1,20 @@
-$(function () {
-    $("#searchButton").on("click", () => {
-        let name = $("#names").val();
-        let ingredients = $("#ingredients").val();
-    
-    $ajax({
-        url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?name=" + name
-    }).done(response => {
-        let res = JSON.parse(response);
+$(function() {
+    $(".button").on("click", () => {
+  
+      let cocktailName = $(".cocktailSelector").val();
+      let cocktailIngredient = $(".cocktailIngredient").val();
+  
+      if (cocktailName == "") {
+          $.ajax({
+              url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktailName
+            })
+      } else {
+          $.ajax({
+              url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=" + cocktailIngredient
+            }) 
+      }
 
-        for(let x in res.1) {
-            name.push(x);
-            data.push(res.bpi[x]);
-        }
-    })
 
-    // $ajax({
-    //     url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?ingredients=" + ingredients 
-    // }).done(response => {
-
-    // })
 
         <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="https://cdn.pixabay.com/photo/2017/07/18/06/21/mojito-2514908_960_720.jpg" alt="Card image cap">
